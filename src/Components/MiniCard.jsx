f6671c2081134cbfe3ba3fd5e2598590037f2675
Jose Marcano <jmarcanoy@gmail.com>
2024-05-08 11:18:46 -0400

    agregando condicionales en el frontend

--- src/Components/MiniCard.jsx
+++ src/Components/MiniCard.jsx
@@ -4,7 +4,20 @@ import { DataContext } from '../App';
 export const MiniCard = ({content}) => {
     const { publicIpAddress, dataApi, nameprovider, countryprovider, regionName,city,region,timezone,latitud,longitud, as } = useContext(DataContext);
     
+
+    function obtenerPrimeraPalabra(content) {
+      if (typeof content?.valor === 'string') {
+        return content?.valor.split(' ')[0] || 'No data';
+      } else if (typeof content?.valor === 'number') {
+        return content?.valor.toString().split(' ')[0] || 'No data';
+      } else {
+        return 'No data';
+      }
+    }
+
+    
   return (
+    
     <div className="bg-cardinf w-[250px] h-[80px] rounded-xl shadow-md p-3 mt-6">
       <div className="flex items-center gap-1">
         <div className="bg-cyan-500 px-[3px] py-[3px] w-[1px] shadow-2xl shadow-green-400 hover:shadow-green-600"></div>
@@ -13,7 +26,9 @@ export const MiniCard = ({content}) => {
         </h2>
       </div>
       <div className="flex items-center gap-2">
-        <h1 className="text-2xl font-semibold text-textoblanco">{content.valor}</h1>
+        <h1 className="text-2xl font-semibold text-textoblanco">{obtenerPrimeraPalabra(content)}
+        
+        </h1>
       </div>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
-import React, {useContext } from "react";
+import React, { useContext } from "react";
 import { MiniCard } from "../Components/MiniCard";
 import { DataContext } from '../App';
+import { Helmet } from "react-helmet";
 
 export const Home = () => {
-  const { publicIpAddress, dataApi, nameprovider, countryprovider, regionName,city,region,timezone,latitud,longitud, as } = useContext(DataContext);
+  const { publicIpAddress, dataApi, nameprovider, countryprovider, regionName, city, region, timezone, latitud, longitud, as } = useContext(DataContext);
   const contentCard = [
     {
       nombre: "Pais",
@@ -42,6 +43,13 @@ export const Home = () => {
 
   return (
     <div className="h-screen flex justify-center items-center">
+      <Helmet>
+        <title>showme my ip - Informacion de mi IP publica</title>
+        <meta name="description" content="Showme my IP - Informacion de mi IP publica" />
+        <meta name="rating" content="General" />
+        <link rel="canonical" href="https://app-letters-convert.vercel.app/" />
+        <meta name="keywords" content="Show me my Ip" />
+      </Helmet>
       <div className="w-[620px] h-[570px] bg-cardgrande rounded-lg p-4 shadow-lg">
         <header className="flex justify-between items-center">
           <h1 className="text-letracardgrande font-semibold uppercase text-[12px]">
@@ -58,10 +66,10 @@ export const Home = () => {
             <div className="bg-green-500 rounded-full px-1 py-1 w-[2px] shadow-2xl shadow-green-400 hover:shadow-green-600"></div>
           </div>
           {/* array */}
-          <div className="grid grid-cols-2">
+          <div className="grid grid-cols-2 justify-items-center align-items-center ">
             {contentCard.map((content) => {
-              return(
-                <MiniCard key={content.nombre} content={content}/>
+              return (
+                <MiniCard key={content.nombre} content={content} />
               )
             })}
           </div>
